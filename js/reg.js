20c74944bc74b6cdf04eefd40adb7a81e6699a5a
vidushig08 <vidushi.gupta2019@vitstudent.ac.in>
2021-05-16 14:58:09 +0530

    Update reg.js

--- js/reg.js
+++ js/reg.js
@@ -1,8 +1,6 @@
 //Reg No array
 
 var regNoArr = [
-    "19BCEguest",
-    "19BCEtest",
     "19BCE0171",
     "19BCE2227",
     "19BCE0922",
@@ -22,8 +20,6 @@ var regNoArr = [
 ];
 
 var emailArr = [
-    "guest@gmail.com",
-    "testuser@gmail.com",
     "dev.sharma2019@vitstudent.ac.in",
     "panshul.jindal2019@vitstudent.ac.in",
     "vidushi.gupta2019@vitstudent.ac.in",
