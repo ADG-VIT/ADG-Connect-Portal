709cce664556ee7e3b79d26bd5a576a5a231c79a
vidushig08 <vidushi.gupta2019@vitstudent.ac.in>
2021-05-16 14:46:50 +0530

    Update reg.js

--- js/reg.js
+++ js/reg.js
@@ -7,7 +7,18 @@ var regNoArr = [
     "19BCE2227",
     "19BCE0922",
     "19BCE0630",
-    "18BCE2018"
+    "18BCE0265",
+    "18BCE2018",
+    "18BCE0796",
+    "18BCE0702",
+    "18BEC0816",
+    "18BCE0770",
+    "18BCE0673",
+    "18BCE2278",
+    "18BCE0761",
+    "18BIT0093",
+    "18BCE2146",
+    "18BCE2026"
 ];
 
 var emailArr = [
@@ -17,7 +28,18 @@ var emailArr = [
     "panshul.jindal2019@vitstudent.ac.in",
     "vidushi.gupta2019@vitstudent.ac.in",
     "anmol.bansal2019@vitstudent.ac.in",
-    "ritik.suryawanshi2018@vitstudent.ac.in"
+    "ishaan.ohri2018@vitstudent.ac.in",
+    "ritik.suryawanshi2018@vitstudent.ac.in",
+    "keshav.periwal2018@vitstudent.ac.in",
+    "akshit.sadana2018@vitstudent.ac.in",
+    "shubhankar.shankar2018@vitstudent.ac.in",
+    "navya.saxena2018@vitstudent.ac.in",
+    "mihir.khare2018@vitstudent.ac.in",
+    "abhishek.dhall2018@vitstudent.ac.in",
+    "divya.jyoti2018@vitstudent.ac.in",
+    "ishika.dubey2018@vitstudent.ac.in",
+    "piyush.bajaj2018@vitstudent.ac.in",
+    "ankit.yadav2018@vitstudent.ac.in"
 ];
 
 
